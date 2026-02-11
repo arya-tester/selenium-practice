5e7c5f4bdac66de22bd56adf84f6e075b493604a
Arya S <arya28051996@gmail.com>
2026-02-11 07:26:03 +0530

    Add Selenium maven project

---
+++ src/test/java/tests/SampleTest.java
@@ -0,0 +1,19 @@
+package tests;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+
+public class SampleTest {
+	WebDriver driver=new ChromeDriver();
+	
+	@Test
+	public void openGoogle() {
+		driver.get("https://www.google.com/");
+		System.out.println("title is "+driver.getTitle());
+		driver.quit();
+	}
+	
+	
+
+}
